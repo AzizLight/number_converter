57c49c0caca6b9482a12d4629e7ca84cc6a7769c
Aziz Light <aziz@azizlight.me>
2011-11-07 16:50:08 +0000

    Minor changes to the base app file

--- lib/number_converter.rb
+++ lib/number_converter.rb
@@ -1,5 +1,5 @@
-require "number_converter/version"
+require_relative "./number_converter/version"
+require_relative "./number_converter/converter"
 
 module NumberConverter
-  # Your code goes here...
 end
