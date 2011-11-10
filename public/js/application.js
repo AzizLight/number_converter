3c9f47032984ac20220ce1228ba23dc24c1e6062
Aziz Light <aziz@azizlight.me>
2011-11-10 19:44:29 +0000

    Setup the application for some JS

---
+++ public/js/application.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  $("form input[type=text]").keyup(function(event) {
+
+    /*
+    // If it's a number
+    if (event.which >= 48 && event.which <= 57)
+    {
+    }
+    // if it's a lowercase letter
+    else if (event.which >= 97 && event.which <= 102 )
+    {
+    }
+    // if it's an uppercase letter
+    else if (event.which >= 65 && event.which <= 90)
+    {
+    }
+    else
+    {
+    }
+    */
+
+    //$.ajax({
+      //type : "POST",
+      //url  : "/",
+      //data : { number : $(this).val() },
+      //success : function(data) {
+        //console.log(data);
+      //},
+
+    //});
+
+  });
+});
